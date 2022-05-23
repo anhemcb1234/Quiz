45dc10943ac938a0060518dca399880119a0e0d3
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-05-23 21:52:28 +0700

    Fix bug

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -66,10 +66,10 @@ function Ranking() {
                       Email user
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      Total number of exams
+                      <p className="text-center">Total number of exams</p>
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      Total point
+                      <p className="text-center">Total point</p>
                     </th>
                   </tr>
                 </thead>
