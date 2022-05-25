7afa9b905e17f648d779846c37203c7459e47df7
HOANG TRUNG TIEN <44317164+anhemcb1234@users.noreply.github.com>
2022-05-25 18:30:17 +0700

    Update LogIn.js

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -35,39 +35,24 @@ const Login = () => {
   return (
     <>
       {showToast ? (
-        <div classNameName="flex  absolute left-0 right-0 top-10  ease-in duration-300 flex-col justify-center">
-          <div
-            classNameName="bg-red-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3"
-            id="static-example"
-            aria-live="assertive"
-            aria-atomic="true"
-            data-mdb-autohide="false"
-          >
-            <div classNameName="bg-red-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-red-500 rounded-t-lg">
-              <p classNameName="font-bold text-white flex items-center">
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  data-icon="info-circle"
-                  classNameName="w-4 h-4 mr-2 fill-current"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
-                  ></path>
-                </svg>
-                ERROR
-              </p>
-            </div>
-            <div className="p-3 bg-red-600 rounded-b-lg break-words text-white">
-              <p className="font-bold">Please check your password and account</p>
-            </div>
+        <div className="flex absolute left-0 right-0 top-10  ease-in duration-300 flex-col justify-center">
+        <div
+          className="bg-red-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3"
+          id="static-example"
+          aria-live="assertive"
+          aria-atomic="true"
+          data-mdb-autohide="false"
+        >
+          <div className="bg-red-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-red-500 rounded-t-lg">
+            <p className="font-bold text-white flex items-center">Error</p>
+          </div>
+          <div className="p-3 bg-red-600 rounded-b-lg break-words text-white">
+            <p className="font-bold">
+              Please check your username or password
+            </p>
           </div>
         </div>
+      </div>
       ) : null}
       <div className="bg-white dark:bg-gray-900">
         <div className="flex justify-center h-screen">
