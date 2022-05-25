712a90a5fe485a0ba30a150b44df44358d0288e9
HOANG TRUNG TIEN <44317164+anhemcb1234@users.noreply.github.com>
2022-05-25 18:02:21 +0700

    Update LogIn.js

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -10,8 +10,7 @@ const Login = () => {
 
   let navigate = useNavigate();
 
-
-  const handlerLogin = async () => {
+  const handlerLogin = async (e) => {
     try {
       setShow(true);
       const reps = await loginServices.Login({
@@ -21,8 +20,7 @@ const Login = () => {
       localStorage.setItem("token", reps.data.token);
       localStorage.setItem("userName", reps.data.username);
       localStorage.setItem("idUser", reps.data.id);
-
-      navigate(`/choose-quiz?userName=${reps.data.username}&id=${reps.data.id}`);
+      navigate(`/choose-quiz`);
     } catch (e) {
       navigate("/");
       setTimeout(() => {
@@ -35,102 +33,135 @@ const Login = () => {
     }
   };
   return (
-        <>
-          {showToast ? (
-            <div className="flex  absolute left-0 right-0 top-10  ease-in duration-300 flex-col justify-center">
-              <div
-                className="bg-red-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3"
-                id="static-example"
-                aria-live="assertive"
-                aria-atomic="true"
-                data-mdb-autohide="false"
-              >
-                <div className="bg-red-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-red-500 rounded-t-lg">
-                  <p className="font-bold text-white flex items-center">
-                    <svg
-                      aria-hidden="true"
-                      focusable="false"
-                      data-prefix="fas"
-                      data-icon="info-circle"
-                      className="w-4 h-4 mr-2 fill-current"
-                      role="img"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
+    <>
+      {showToast ? (
+        <div classNameName="flex  absolute left-0 right-0 top-10  ease-in duration-300 flex-col justify-center">
+          <div
+            classNameName="bg-red-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3"
+            id="static-example"
+            aria-live="assertive"
+            aria-atomic="true"
+            data-mdb-autohide="false"
+          >
+            <div classNameName="bg-red-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-red-500 rounded-t-lg">
+              <p classNameName="font-bold text-white flex items-center">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="info-circle"
+                  classNameName="w-4 h-4 mr-2 fill-current"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
+                  ></path>
+                </svg>
+                ERROR
+              </p>
+            </div>
+            <div className="p-3 bg-red-600 rounded-b-lg break-words text-white">
+              <p className="font-bold">Please check your password and account</p>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      <div className="bg-white dark:bg-gray-900">
+        <div className="flex justify-center h-screen">
+          <div
+            className="hidden bg-cover lg:block lg:w-2/3"
+            style={{backgroundImage: `url("https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`}}
+          >
+            <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+              <div>
+                <h2 className="text-4xl font-bold text-white">Brand</h2>
+
+                <p className="max-w-xl mt-3 text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
+                  autem ipsa, nulla laboriosam dolores, repellendus perferendis
+                  libero suscipit nam temporibus molestiae
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+            <div className="flex-1">
+              <div className="text-center">
+                <h2 className="text-4xl font-bold text-center text-gray-700 dark:text-white">
+                  Brand
+                </h2>
+
+                <p className="mt-3 text-gray-500 dark:text-gray-300">
+                  Sign in to access your account
+                </p>
+              </div>
+
+              <div className="mt-8">
+                  <div>
+                    <label
+                      for="UserName"
+                      className="block mb-2 text-sm text-gray-600 dark:text-gray-200"
                     >
-                      <path
-                        fill="currentColor"
-                        d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
-                      ></path>
-                    </svg>
-                    ERROR
-                  </p>
-                </div>
-                <div class="p-3 bg-red-600 rounded-b-lg break-words text-white">
-                  <p class="font-bold">
-                    Please check your password and account
-                  </p>
-                </div>
-              </div>
-            </div>
-          ) : null}
+                      Username
+                    </label>
+                    <input
+                      type="text"
+                      name="UserName"
+                      id="UserName"
+                      placeholder="User Name"
+                      onChange={(e) => setUserName(e.target.value)}
+                      className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                  </div>
 
-          <section className="h-screen ">
-            <div className="px-6 h-full text-gray-800">
-              <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
-                <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                    className="w-full"
-                    alt="Sample image"
-                  />
-                </div>
-                <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-                  <form>
-                    <div className="mb-6">
-                      <h1 className="text-4xl font-bold text-center ">Login</h1>
-                    </div>
-
-                    <div className="mb-6">
-                      <input
-                        type="text"
-                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        id="exampleFormControlInput"
-                        placeholder="Email address"
-                        onChange={(e) => setUserName(e.target.value)}
-                      />
-                    </div>
-
-                    <div className="mb-6">
-                      <input
-                        type="password"
-                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        id="exampleFormControlInput2"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </div>
-
-                    <div className="text-center lg:text-left">
-                      <button
-                        onClick={() => handlerLogin()}
-                        type="button"
-                        className="inline-block w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  <div className="mt-6">
+                    <div className="flex justify-between mb-2">
+                      <label
+                        for="password"
+                        className="text-sm text-gray-600 dark:text-gray-200"
                       >
-                        Login
-                      </button>
-                      <p className="text-sm font-semibold text-center mt-4 pt-1 mb-0">
-                        Don't have an account?
-                        <Link to="/signup">
-                          <button className="underline ml-1">Register</button>
-                        </Link>
-                      </p>
+                        Password
+                      </label>
                     </div>
-                  </form>
-                </div>
+
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Your Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                  </div>
+
+                  <div className="mt-6">
+                    <button onClick={() => handlerLogin()} className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                      Sign in
+                    </button>
+                  </div>
+
+                <p className="mt-6 text-sm text-center text-gray-400">
+                  Don&#x27;t have an account yet?{" "}
+                  <Link to="/signup">
+                  <button
+                    href="#"
+                    className="text-blue-500 focus:outline-none focus:underline hover:underline"
+                  >
+                    Sign up
+                  </button>
+                    </Link>
+                  .
+                </p>
               </div>
             </div>
-          </section>
-        </>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
