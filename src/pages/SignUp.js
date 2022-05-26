ebc921fcbbfa512c21d7b0129b7efa8b553f9d7c
HOANG TRUNG TIEN <44317164+anhemcb1234@users.noreply.github.com>
2022-05-26 23:35:58 +0700

    Update SignUp.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -63,7 +63,7 @@ const SignUp = () => {
             </svg>
           </div>
           <div className="ml-3 text-sm font-normal">
-            Login failed. Please check your username and password
+            Please fill the form correctly
           </div>
           <button
             type="button"
@@ -111,7 +111,7 @@ const SignUp = () => {
               ></path>
             </svg>
           </div>
-          <div className="ml-3 text-sm font-normal">Login successfully.</div>
+          <div className="ml-3 text-sm font-normal">Sign up success</div>
           <button
             type="button"
             className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
@@ -136,13 +136,13 @@ const SignUp = () => {
       ) : (
         ""
       )}
-      <div class="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
+      <div className="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
         <div
-          class="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden"
+          className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden"
           style={{ maxWidth: "1000px" }}
         >
-          <div class="md:flex w-full">
-            <div class="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
+          <div className="md:flex w-full">
+            <div className="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
               <svg
                 id="a87032b8-5b37-4b7e-a4d9-4dbfbe394641"
                 data-name="Layer 1"
@@ -347,71 +347,71 @@ const SignUp = () => {
                 />
               </svg>
             </div>
-            <div class="w-full md:w-1/2 py-10 px-5 md:px-10">
-              <div class="text-center mb-10">
-                <h1 class="font-bold text-3xl text-gray-900">REGISTER</h1>
+            <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
+              <div className="text-center mb-10">
+                <h1 className="font-bold text-3xl text-gray-900">REGISTER</h1>
                 <p>Enter your information to register</p>
               </div>
               <div>
-                <div class="flex -mx-3">
-                  <div class="w-full px-3 mb-5">
-                    <label for="" class="text-xs font-semibold px-1">
-                      Full name
+                <div className="flex -mx-3">
+                  <div className="w-full px-3 mb-5">
+                    <label for="" className="text-xs font-semibold px-1">
+                      User name
                     </label>
-                    <div class="flex">
-                      <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                        <i class="mdi mdi-account-outline text-gray-400 text-lg"></i>
+                    <div className="flex">
+                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                        <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
                       </div>
                       <input
                         onChange={(e) => setUserName(e.target.value)}
                         type="text"
-                        class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="Smith"
                       />
                     </div>
                   </div>
                 </div>
-                <div class="flex -mx-3">
-                  <div class="w-full px-3 mb-5">
-                    <label for="" class="text-xs font-semibold px-1">
+                <div className="flex -mx-3">
+                  <div className="w-full px-3 mb-5">
+                    <label for="" className="text-xs font-semibold px-1">
                       Email
                     </label>
-                    <div class="flex">
-                      <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                        <i class="mdi mdi-email-outline text-gray-400 text-lg"></i>
+                    <div className="flex">
+                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                        <i className="mdi mdi-email-outline text-gray-400 text-lg"></i>
                       </div>
                       <input
                         onChange={(e) => setEmail(e.target.value)}
                         type="email"
-                        class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="johnsmith@example.com"
                       />
                     </div>
                   </div>
                 </div>
-                <div class="flex -mx-3">
-                  <div class="w-full px-3 mb-12">
-                    <label for="" class="text-xs font-semibold px-1">
+                <div className="flex -mx-3">
+                  <div className="w-full px-3 mb-12">
+                    <label for="" className="text-xs font-semibold px-1">
                       Password
                     </label>
-                    <div class="flex">
-                      <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                        <i class="mdi mdi-lock-outline text-gray-400 text-lg"></i>
+                    <div className="flex">
+                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                        <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
                       </div>
                       <input
                         onChange={(e) => setPassword(e.target.value)}
                         type="password"
-                        class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="************"
                       />
                     </div>
                   </div>
                 </div>
-                <div class="flex -mx-3">
-                  <div class="w-full px-3 mb-5">
+                <div className="flex -mx-3">
+                  <div className="w-full px-3 mb-5">
                     <button
                       onClick={() => handlerSingUp()}
-                      class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"
+                      className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"
                     >
                       REGISTER NOW
                     </button>
