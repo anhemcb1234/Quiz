983b2c16a77775708d48bae49b0655515886b18c
HOANG TRUNG TIEN <44317164+anhemcb1234@users.noreply.github.com>
2022-05-26 16:34:43 +0700

    Update LogIn.js

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -5,14 +5,13 @@ import { loginServices } from "../services/loginServices";
 const Login = () => {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [show, setShow] = useState(false);
   const [showToast, setShowToast] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   let navigate = useNavigate();
 
   const handlerLogin = async (e) => {
     try {
-      setShow(true);
       const reps = await loginServices.Login({
         username,
         password,
@@ -20,12 +19,12 @@ const Login = () => {
       localStorage.setItem("token", reps.data.token);
       localStorage.setItem("userName", reps.data.username);
       localStorage.setItem("idUser", reps.data.id);
-      navigate(`/choose-quiz`);
+      setShowLogin(true);
+      setTimeout(() => {
+        navigate(`/choose-quiz`);
+      }, 2000);
     } catch (e) {
       navigate("/");
-      setTimeout(() => {
-        setShow(false);
-      }, 2000);
       setShowToast(true);
       setTimeout(() => {
         setShowToast(false);
@@ -35,30 +34,103 @@ const Login = () => {
   return (
     <>
       {showToast ? (
-        <div className="flex absolute left-0 right-0 top-10  ease-in duration-300 flex-col justify-center">
         <div
-          className="bg-red-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3"
-          id="static-example"
-          aria-live="assertive"
-          aria-atomic="true"
-          data-mdb-autohide="false"
+          id="toast-warning"
+          class="flex absolute top-10 right-3 items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+          role="alert"
         >
-          <div className="bg-red-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-red-500 rounded-t-lg">
-            <p className="font-bold text-white flex items-center">Error</p>
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </div>
-          <div className="p-3 bg-red-600 rounded-b-lg break-words text-white">
-            <p className="font-bold">
-              Please check your username or password
-            </p>
+          <div className="ml-3 text-sm font-normal">
+            Login failed. Please check your username and password
           </div>
+          <button
+            type="button"
+            className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            data-dismiss-target="#toast-success"
+            aria-label="Close"
+            onClick={() => setShowToast(false)}
+          >
+            <span className="sr-only">Close</span>
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
-      </div>
-      ) : null}
+      ) : ''}
+      {showLogin ? (
+        <div
+          id="toast-success"
+          className="flex  absolute top-10 right-3 items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+          role="alert"
+        >
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <div className="ml-3 text-sm font-normal">Login successfully.</div>
+          <button
+            type="button"
+            className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            data-dismiss-target="#toast-success"
+            aria-label="Close"
+          >
+            <span className="sr-only">Close</span>
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
       <div className="bg-white dark:bg-gray-900">
         <div className="flex justify-center h-screen">
           <div
             className="hidden bg-cover lg:block lg:w-2/3"
-            style={{backgroundImage: `url("https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`}}
+            style={{
+              backgroundImage: `url("https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`,
+            }}
           >
             <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
               <div>
@@ -86,59 +158,62 @@ const Login = () => {
               </div>
 
               <div className="mt-8">
-                  <div>
+                <div>
+                  <label
+                    htmlFor="UserName"
+                    className="block mb-2 text-sm text-gray-600 dark:text-gray-200"
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    name="UserName"
+                    id="UserName"
+                    placeholder="User Name"
+                    onChange={(e) => setUserName(e.target.value)}
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
+
+                <div className="mt-6">
+                  <div className="flex justify-between mb-2">
                     <label
-                      for="UserName"
-                      className="block mb-2 text-sm text-gray-600 dark:text-gray-200"
+                      htmlFor="password"
+                      className="text-sm text-gray-600 dark:text-gray-200"
                     >
-                      Username
+                      Password
                     </label>
-                    <input
-                      type="text"
-                      name="UserName"
-                      id="UserName"
-                      placeholder="User Name"
-                      onChange={(e) => setUserName(e.target.value)}
-                      className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                    />
                   </div>
 
-                  <div className="mt-6">
-                    <div className="flex justify-between mb-2">
-                      <label
-                        for="password"
-                        className="text-sm text-gray-600 dark:text-gray-200"
-                      >
-                        Password
-                      </label>
-                    </div>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Your Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
 
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="Your Password"
-                      onChange={(e) => setPassword(e.target.value)}
-                      className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                    />
-                  </div>
-
-                  <div className="mt-6">
-                    <button onClick={() => handlerLogin()} className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                      Sign in
-                    </button>
-                  </div>
+                <div className="mt-6">
+                  <button
+                    onClick={() => handlerLogin()}
+                    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  >
+                    Sign in
+                  </button>
+                </div>
 
                 <p className="mt-6 text-sm text-center text-gray-400">
                   Don&#x27;t have an account yet?{" "}
                   <Link to="/signup">
-                  <button
-                    href="#"
-                    className="text-blue-500 focus:outline-none focus:underline hover:underline"
-                  >
-                    Sign up
-                  </button>
-                    </Link>
+                    <button
+                      href="#"
+                      className="text-blue-500 focus:outline-none focus:underline hover:underline"
+                    >
+                      Sign up
+                    </button>
+                  </Link>
                   .
                 </p>
               </div>
