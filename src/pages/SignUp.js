238a0eeda74d150441aeac9f260acf0a7fc3ad8a
HOANG TRUNG TIEN <44317164+anhemcb1234@users.noreply.github.com>
2022-05-26 16:35:00 +0700

    Update SignUp.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -19,72 +19,119 @@ const SignUp = () => {
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
       )
     ) {
-      setShowToast(false);
-      setShowLogin(true);
+      setShowToast(true);
       setTimeout(() => {
-        setShowLogin(false);
-      }, 3000);
+        setShowToast(false)
+      },5000)
       return;
     }
     try {
+      setShowLogin(true);
       await signServices.SignUp({
         username,
         password,
         email,
       });
-      setShowToast(true);
       setTimeout(() => {
-        setShowToast(false);
         navigate("/");
       }, 2000);
     } catch (e) {
-      alert("Sign up fail");
       console.log(e);
     }
-  };
-  const handlerTest = () => {
-    console.log(password);
-    console.log(username);
-    console.log(email);
   };
   return (
     <>
       {showToast ? (
-        <div className="flex absolute left-0 right-0 top-10  ease-in duration-300 flex-col justify-center">
-          <div
-            className="bg-green-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3"
-            id="static-example"
-            aria-live="assertive"
-            aria-atomic="true"
-            data-mdb-autohide="false"
-          >
-            <div className="bg-green-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-green-500 rounded-t-lg">
-              <p className="font-bold text-white flex items-center">SUCCESS</p>
-            </div>
-            <div className="p-3 bg-green-600 rounded-b-lg break-words text-white">
-              <p className="font-bold">Sign up success</p>
-            </div>
+        <div
+          id="toast-warning"
+          class="flex absolute items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+          role="alert"
+          style={{top:'13%',left:'7%'}}
+        >
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </div>
+          <div className="ml-3 text-sm font-normal">
+            Login failed. Please check your username and password
+          </div>
+          <button
+            type="button"
+            className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            data-dismiss-target="#toast-success"
+            aria-label="Close"
+            onClick={() => setShowToast(false)}
+          >
+            <span className="sr-only">Close</span>
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
-      ) : showLogin ? (
-        <div className="flex absolute left-0 right-0 top-10  ease-in duration-300 flex-col justify-center">
-          <div
-            className="bg-red-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3"
-            id="static-example"
-            aria-live="assertive"
-            aria-atomic="true"
-            data-mdb-autohide="false"
-          >
-            <div className="bg-red-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-red-500 rounded-t-lg">
-              <p className="font-bold text-white flex items-center">Error</p>
-            </div>
-            <div className="p-3 bg-red-600 rounded-b-lg break-words text-white">
-              <p className="font-bold">
-                Username, password must be more than 6 characters and Email must
-                be in the correct format
-              </p>
-            </div>
+      ) : (
+        ""
+      )}
+      {showLogin ? (
+        <div
+          id="toast-success"
+          className="flex absolute items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+          role="alert"
+          style={{top:'13%',left:'7%'}}
+        >
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </div>
+          <div className="ml-3 text-sm font-normal">Login successfully.</div>
+          <button
+            type="button"
+            className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            data-dismiss-target="#toast-success"
+            aria-label="Close"
+          >
+            <span className="sr-only">Close</span>
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
       ) : (
         ""
@@ -370,18 +417,18 @@ const SignUp = () => {
                     </button>
                   </div>
                 </div>
-                  <p className="mt-6 text-sm text-center text-gray-400">
-                    Already have an account??{" "}
-                    <Link to="/">
-                      <button
-                        href="#"
-                        className="text-blue-500 focus:outline-none focus:underline hover:underline"
-                      >
-                        Sign in
-                      </button>
-                    </Link>
-                    .
-                  </p>
+                <p className="mt-6 text-sm text-center text-gray-400">
+                  Already have an account??{" "}
+                  <Link to="/">
+                    <button
+                      href="#"
+                      className="text-blue-500 focus:outline-none focus:underline hover:underline"
+                    >
+                      Sign in
+                    </button>
+                  </Link>
+                  .
+                </p>
               </div>
             </div>
           </div>
